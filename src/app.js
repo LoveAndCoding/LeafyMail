6f1ca3518cc824783cdd7f1c59c062657e680fa6
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-04-27 16:02:56 -0700

    Switched to JQuery
    Will be helpful as I expand out and want to do more things. May also try and do something MVC, which may or may not require or replace jQuery, but for now I'll use it to get things going and figure that stuff out later

--- src/app.js
+++ src/app.js
@@ -3,24 +3,30 @@ var gui = require('nw.gui'),
 
 // win.showDevTools();
 
-window.addEventListener('load', function() {
-	document.getElementById('close').addEventListener('click', function () {
-		win.close();
-	}, false);
-	document.getElementById('dev-tools').addEventListener('click', function () {
-		win.showDevTools();
-	}, false);
-	document.getElementById('maximize').addEventListener('click', function () {
-		if(currWinMode == 'maximized')
-			win.unmaximize();
-		else
-			win.maximize();
-	}, false);
-	document.getElementById('minimize').addEventListener('click', function () {
-		win.minimize();
-	}, false);
-	document.getElementById('refresh').addEventListener('click', function () {
-		saveWindowState();
-		window.location.reload();
-	}, false);
+$('#close').click(function () {
+	win.close();
+});
+$('#dev-tools').click(function () {
+	win.showDevTools();
+});
+$('#maximize').click(function () {
+	if(currWinMode == 'maximized') {
+		win.unmaximize();
+		$(document.body).hide().show();
+	} else
+		win.maximize();
+});
+$('#minimize').click(function () {
+	win.minimize();
+});
+$('#refresh').click(function () {
+	saveWindowState();
+	window.location.reload();
+});
+
+$('body > .loading-pane').remove();
+
+$('#primary-nav a').click(function () {
+	$('#primary-nav .current').removeClass('current');
+	$(this).addClass('current');
 });
